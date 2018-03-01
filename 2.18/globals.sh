557d039f926d1dd1ac0817352390ce7828235299
Tim Sutton <tim@kartoza.com>
2018-03-01 23:33:08 +0200

    Update globals.sh

--- 2.18/globals.sh
+++ 2.18/globals.sh
@@ -2,10 +2,10 @@
 ##
 ## GLOBALS for use by build, test, run etc scripts
 ##
-BUGFIX=12
+BUGFIX=17
 MINOR=18
 MAJOR=2
-TESTPORT=9999
+TESTPORT=9909
 # $$ at the end will add a random number to the server
 TESTSERVER=qgis-server-test$$
 URL=http://localhost:${TESTPORT}/
