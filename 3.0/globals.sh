8e5c1478f9305fd0730f69a63eef1cbbdbcd1387
Tim Sutton <tim@kartoza.com>
2018-05-31 00:12:14 +0200

    Use QGIS3 save project for qgis 3 server tests

--- 3.0/globals.sh
+++ 3.0/globals.sh
@@ -9,6 +9,6 @@ TESTPORT=9909
 # $$ at the end will add a random number to the server
 TESTSERVER=qgis-server-test$$
 URL=http://localhost:${TESTPORT}/
-PROJECT=`pwd`/../project
+PROJECT=`pwd`/../project-qgis3
 TESTIMAGE=/tmp/test$$.jpg
 
